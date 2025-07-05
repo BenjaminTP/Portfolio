32cd01a8f0430db3f77cefc86f008fe2f7025b55
BenjaminTP <benjamintranpugh@gmail.com>
2025-07-05 01:01:43 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,21 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let count = 0;
-    let button = document.querySelector('#button1');
-    button.addEventListener('click', function() {
-        if (count % 2 != 0) {
-            button.style.backgroundColor = '';
-        } else {
-            button.style.backgroundColor = 'green';
-        }
-        count++;
-    });
-    button.addEventListener('mouseover', function() {
-        if (count % 2 === 0) {
-            document.querySelector('#feedback1').innerHTML = 'PRESS IT';
-        }
-    });
-    button.addEventListener('mouseout', function() {
-        document.querySelector('#feedback1').innerHTML = '';
-    });
-});
-
+// document.addEventListener("DOMContentLoaded", function() {
+//     let card = document.querySelector("#project1")
+//     card.addEventListener('click', function() {
+//         card.style.backgroundColor = 'red';
+//     });
+// });
